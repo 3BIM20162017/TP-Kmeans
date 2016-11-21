a54bf7debf9610c843720fb5d5916f3dd12a4c19
totoetco <thomas.breniere@insa-lyon.fr>
2016-11-21 14:19:29 +0100

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -4,7 +4,7 @@ import matplotlib
 matplotlib.style.use('ggplot')
 
 from pandas.tools.plotting import parallel_coordinates
-#from pandas.tools.plotting import radviz
+#from pandas.tools.plotting import radviz #(other choice of visualization, if used, uncomment radviz(data, 'Name')
 
 
 data = pd.read_csv('iris.csv')
